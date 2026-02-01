6fb8a4ec680f700f9d79c583d2fe939ed2ae60a0
Miguel015 <miguelandressuarezp@gmail.com>
2026-02-01 17:54:18 -0500

    Enable CORS for frontend on Vercel

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
